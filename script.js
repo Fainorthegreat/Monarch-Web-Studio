c34d45398dab64885a943755e136b8b1b9a9f492
Fainorthegreat <57128512+Fainorthegreat@users.noreply.github.com>
2020-01-22 17:28:54 +0300

    Burger menu rdy for Tablet

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   
     $('.fly-elements__fly-selector, .fly-elements__fly-teg,  .fly-elements__fly-slesh, .fly-elements__fly-div2, .fly-elements__fly-div').removeClass('anim anim2');
+
     var mySwiper = new Swiper ('.swiper-container', {
       loop: true,
       slidesPerView: 2,
@@ -26,20 +27,28 @@ $(document).ready(function(){
       },
     });
     
-    $('#menu').on('click', function() {
-      $('.header_nav_mobile').toggle();   
-})
 
-$('#menu').on('click', function(){
-    if ($('body').hasClass('hidden')) // *HASCLASS - ОЧЕНЬ УДОБНАЯ ФУНКЦИЯ ПРОВЕРКИ КЛАССА, А ТАКЖЕ ВКЛЮЧЕНИЯ И ВЫКЛЮЧЕНИЯ 
-    {
-    $('body').removeClass('hidden');}
-    else {
-    $('body').addClass('hidden');
-    }
-})
+$(function(){
+	
+	$('#nav-icon3').on('click', function(){
+		
+		$('.header__mob-nav').slideToggle(300, function(){
+			
+			if($(this).css('display') === 'none'){
+				$(this).removeAttr('style');
+		}
+		
+  });
+    
+	
+ });
+	
+});
+
+$('#nav-icon3').click(function(){
+  $(this).toggleClass('open');
+});
+
 
   });
-
-  
   
