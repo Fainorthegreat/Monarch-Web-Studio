eab031db7084621548af929123030cb40efc535c
Fainorthegreat <57128512+Fainorthegreat@users.noreply.github.com>
2020-02-03 10:11:21 +0300

    update

--- script.js
+++ script.js
@@ -27,6 +27,12 @@ $(document).ready(function(){
       },
     });
     
+    $(".header__content___left").on("click","a", function (event) {
+      event.preventDefault();
+      var id  = $(this).attr('href'),
+          top = $(id).offset().top;
+      $('body,html').animate({scrollTop: top}, 800);
+  });
 
 $(function(){
   $('#nav-icon3').on('click', function(){
