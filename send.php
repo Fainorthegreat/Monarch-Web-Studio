5e2ec4a256f8abb19af42871d304a4a6dd3d3478
Fainorthegreat <57128512+Fainorthegreat@users.noreply.github.com>
2020-02-29 19:22:34 +0300

    up

--- send.php
+++ send.php
@@ -3,22 +3,22 @@ $fname = $_POST['fname'];
 $email = $_POST['email'];
 $tel = $_POST['tel'];
 
-$fname = htmlspecialchars($fio);
+$fname = htmlspecialchars($fname);
 $email = htmlspecialchars($email);
 
-$fname = urldecode($fio);
+$fname = urldecode($fname);
 $email = urldecode($email);
 
-$fname = trim($fio);
+$fname = trim($fname);
 $email = trim($email);
 
-echo $fio;
+echo $fname;
 echo "<br>";
 echo $email;
 
 mail("fainorthegreat3@gmail.com", "Заявка с сайта", "ФИО:".$fname.". E-mail: ".$email ,"From: send@a0391989.xsph.ru\r\n");
 
-if (mail("example@mail.ru", "Заказ с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+if (mail("example@mail.ru", "Заказ с сайта", "ФИО:".$fname.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
  {
     echo "сообщение успешно отправлено";
 } else {
