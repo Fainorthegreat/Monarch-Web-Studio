3eba36c816a895074ac298ad41eb4aa9483c4c5b
Fainorthegreat <57128512+Fainorthegreat@users.noreply.github.com>
2020-02-29 19:34:05 +0300

    up

--- send.php
+++ send.php
@@ -16,13 +16,13 @@ echo $fname;
 echo "<br>";
 echo $email;
 
-mail("fainorthegreat3@gmail.com", "Заявка с сайта", "ФИО:".$fname.". E-mail: ".$email ," Номер телефона : ".$tel ,"From: send@a0391989.xsph.ru\r\n");
+mail("fainorthegreat3@gmail.com", "Заявка с сайта", "ФИО:".$fname.". E-mail: ".$email." Номер телефона : ".$tel ,"From: send@a0391989.xsph.ru\r\n");
 
-if (mail("example@mail.ru", "Заказ с сайта", "ФИО:".$fname.". E-mail: ".$email , " Номер телефона : ".$tel , "From: example2@mail.ru \r\n"))
+if (mail("fainorthegreat3@gmail.com", "Заказ с сайта", "ФИО:".$fname.". E-mail: ".$email." Номер телефона : ".$tel , "From: example2@mail.ru \r\n"))
  {
     echo " сообщение успешно отправлено";
 } else {
-    echo "при отправке сообщения возникли ошибки";
+    echo " при отправке сообщения возникли ошибки";
 }
 
 ?>
